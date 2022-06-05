3e4da2a0ddd6aa111d7e577852a55d4fdbbee4f4
shehzad ahmad <apple@192.168.187.159>
2022-06-05 15:39:39 +0500

    first commit

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ require("./middleware/common")(app);
 
 // Routes
 app.use("/api", require("./routes"));
+app.get("/", (req, res) => {
+  res.json({ message: "welcomre! app working" });
+});
 
 // Server
 app.listen(port, () => {
