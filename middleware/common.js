3ea60b6a47c05e1a3eb6707b1c78810c19b108b6
shehzad ahmad <apple@192.168.217.159>
2022-06-22 00:30:10 +0500

    arviewer completed

--- middleware/common.js
+++ middleware/common.js
@@ -7,10 +7,10 @@ const cors = require("cors");
 module.exports = (app) => {
   app.use(cors());
   app.use(logger("dev"));
-  app.use(express.json({ limit: "50mb" }));
+  app.use(express.json({ limit: 52428800 }));
   app.use(
     express.urlencoded({
-      limit: "50mb",
+      limit: 52428800,
       extended: true,
     })
   );
